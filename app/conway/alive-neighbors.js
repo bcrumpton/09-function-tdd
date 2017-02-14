95529d7019c3c64031f83aa3d5cd79b73da6cdb8
Brannon Crumpton <bcrump@daniel-johnsons-macbook-pro.local>
2017-02-13 18:29:47 -0600

    functions for the game of life developed

--- app/conway/alive-neighbors.js
+++ app/conway/alive-neighbors.js
@@ -1,1 +1,11 @@
-export default function () {}
+export default function aliveNeighbors(a) {
+  let alive = 0;
+
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i]) {
+      alive += 1;
+    }
+  }
+
+  return alive;
+}
--- app/conway/should-stay-alive.js
+++ app/conway/should-stay-alive.js
@@ -1,3 +1,9 @@
 import aliveNeighbors from './alive-neighbors';
 
-export default function () {}
+export default function shouldStayAlive(a) {
+  if (aliveNeighbors(a) >= 1 && aliveNeighbors(a) <= 3) {
+    return true;
+  }
+
+  return false;
+}
